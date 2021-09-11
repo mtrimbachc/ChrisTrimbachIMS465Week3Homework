edef7352027fc31633b4c774e83253d7272c66fb
Michael Chris Trimbach <trimbamc@miamioh.edu>
2021-09-11 19:52:12 -0400

    Paddle Controls
    The player can now control the paddle's left and right position using the "A" / Left Arrow and "D" / Right Arrow keys respectively.

--- Assets/Scripts/Paddle/Paddle.cs
+++ Assets/Scripts/Paddle/Paddle.cs
@@ -3,7 +3,30 @@
 public class Paddle : MonoBehaviour
 {
     //TODO
-    // Move paddle left and right using keyboard keys, mapping is up to you
     // Paddle should be able to launch the ball upon space bar being pressed
     // A launched ball will then bounce around, changing its direction upon any collision
+
+    //Complete
+    // Move paddle left and right using keyboard keys, mapping is up to you
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    
+
+    public void Update()
+    {
+        // Movement Code
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed, Space.World);
+        }
+
+        var xpos = Mathf.Clamp(transform.position.x, -10.5f, 10.5f);
+        transform.position = new Vector3(xpos, transform.position.y, transform.position.z);
+    }
 }
