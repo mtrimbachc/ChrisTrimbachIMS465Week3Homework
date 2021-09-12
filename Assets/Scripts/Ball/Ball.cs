92cfa402b94fefe66259b1835985aa2b657a7b1a
Michael Chris Trimbach <trimbamc@miamioh.edu>
2021-09-11 21:07:08 -0400

    Ball Launching
    The ball is now launched from the paddle when the player presses the spacebar, and only moves once it has been launched.

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -4,7 +4,26 @@ public class Ball : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody2D rigidBody = null;
+    private float moveSpeed = 1f;
+
+    private bool launched = false;
+    private Vector3 momentumVector = Vector3.up;
 
     //TODO: Add code to move ball along with code to delete pieces upon colliding with one
-    //Ball should only move once its been launched
+
+    // Complete
+    // Ball should only move once its been launched
+
+    public void Update()
+    {
+        if (launched)
+        {
+            transform.Translate(momentumVector * Time.deltaTime * moveSpeed, Space.World);
+        }
+    }
+
+    public void Launch()
+    {
+        launched = true;
+    }
 }
--- Assets/Scripts/Paddle/Paddle.cs
+++ Assets/Scripts/Paddle/Paddle.cs
@@ -3,15 +3,25 @@
 public class Paddle : MonoBehaviour
 {
     //TODO
-    // Paddle should be able to launch the ball upon space bar being pressed
     // A launched ball will then bounce around, changing its direction upon any collision
 
     //Complete
     // Move paddle left and right using keyboard keys, mapping is up to you
+    // Paddle should be able to launch the ball upon space bar being pressed
 
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private GameObject Ball;
+    private GameObject BallLauncher;
+    private GameObject activeBall;
+    private bool ballLaunched = false;
 
-    
+    private void Start()
+    {
+        BallLauncher = GameObject.Find("BallLauncher");
+
+        SpawnBall();
+    }
+
 
     public void Update()
     {
@@ -28,5 +38,23 @@ public class Paddle : MonoBehaviour
 
         var xpos = Mathf.Clamp(transform.position.x, -10.5f, 10.5f);
         transform.position = new Vector3(xpos, transform.position.y, transform.position.z);
+
+        if (activeBall != null && !ballLaunched)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Ball ball = activeBall.GetComponent<Ball>();
+                ball.Launch();
+            }
+        }
+
+    }
+
+    public void SpawnBall()
+    {
+        if (Ball != null && BallLauncher != null)
+        {
+            activeBall = Instantiate(Ball, BallLauncher.transform);
+        }
     }
 }
